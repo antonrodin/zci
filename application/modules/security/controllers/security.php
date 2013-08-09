7b93943c0754b3408c0353c60503322aa9dfb99e
Anton Zekeriev Rodin <antonzekeriev@gmail.com>
2013-08-09 15:50:08 +0200

    Add some code comments

--- application/modules/security/controllers/security.php
+++ application/modules/security/controllers/security.php
@@ -15,18 +15,30 @@ class Security extends MX_Controller {
         redirect($this->_login, "refresh");
     }
     
+    /**
+     * If not logged IN redirect to HOME
+     */
     public function is_logged_in() {
         if (!$this->session->userdata("is_logged_in")) {
             redirect($this->_login, "refresh");
         }
     }
     
+    /**
+     * If Usser is Logged, redirect TO $redirect
+     * @param <string> $redirect PATH "admin/user/all" where to redirect
+     */
     public function is_logged_in_redirect($redirect) {
         if ($this->session->userdata("is_logged_in")) {
             redirect($redirect, "refresh");
         }
     }
     
+    /**
+     * Encrypt PASSWORD. You should change this if you have better security. Check SHA1 or something like that
+     * @param <stiong> $string String to encrypt
+     * @return <strong> Encrypted password
+     */
     public function encrypt($string) {
         return md5($string);
     }
